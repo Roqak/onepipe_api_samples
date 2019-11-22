fa9d829eb736234e2df44d82c1867a06a8de57ce
akinkunmi_olu <akin@lendsqr.com>
2019-11-22 20:44:08 +0100

    Initial commit

---
+++ account_info.php
@@ -0,0 +1,76 @@
+<?php
+$baseURL = 'https://api.onepipe.io';
+$api_key = "";
+$secret_key = "";
+// generate unique transaction reference
+function generate_txn_ref( ) {
+    global $gen_txn_ref;
+
+ $characters = array(
+ "A","B","C","D","E","F","G","H","J","K","L","M",
+ "N","P","Q","R","S","T","U","V","W","X","Y","Z",
+ "1","2","3","4","5","6","7","8","9");
+ 
+ $keys = array();
+ while(count($keys) < 6) {//10 xters
+     $x = mt_rand(0, count($characters)-1);
+     if(!in_array($x, $keys)) {
+        $keys[] = $x;
+     }
+ }
+foreach($keys as $key){
+    $random_chars .= $characters[$key];
+ }
+
+$gen_txn_ref = "CICO_".$random_chars;
+return $gen_txn_ref;
+
+ };
+    
+$ref = generate_txn_ref();
+$signature = md5($ref.";".$secret_key);
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $baseURL."/v1/lookup/accountinfo",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+CURLOPT_POSTFIELDS => '{
+    "request_ref":"'.$ref.'",
+      "transaction": {
+          "account_number": "6234784766",
+          "bank_code": "070",
+          "customer":{
+              "customer_ref": "6234784766",
+              "firstname": "Ope",
+                "surname": "Adeoye",
+              "email": "opeadeoye@gmail.com",
+              "mobile_no": "2348022221412"
+          }
+    }
+  }',
+  CURLOPT_HTTPHEADER => array(
+    "Accept: */*",
+    "Accept-Encoding: gzip, deflate",
+    "Authorization: Bearer ".$api_key,
+    "Cache-Control: no-cache",
+    "Content-Type: application/json",
+    "Signature: ".$signature,
+    "User-Agent: PostmanRuntime/7.19.0",
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  print_r($response);
+}
